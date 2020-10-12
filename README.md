30517b8cf05042ec0bb82efbf1817880fbca1bb4
Ahmet Ömer <ahmetomerv@gmail.com>
2020-10-12 04:13:16 +0300

    added files, hello world

---
+++ README.md
@@ -0,0 +1,31 @@
+# Clean Twitter
+A browser extension to remove the unwanted Tweets from your timeline, such as Liked Tweets, Retweets and Reply Tweets.
+
+### To install and use it in your local machine:
+
+- `$ git clone https://github.com/ahmetomerv/twitter-received-reply-remover.git`
+
+- `$ cd twitter-received-reply-remover`
+
+- `$ npm install`
+
+### To serve and watch for changes:
+
+- `$ npm run {browser}-watch`
+
+### To build:
+
+- `$ npm run {browser}-build`
+
+### To load the extension in Chrome
+
+- Go to `chrome://extensions` in Chrome
+
+- Enable "Developer Mode"
+
+- Click "Load unpacked"
+
+- Select the `build/chrome` folder in `twitter-received-reply-remover` project folder
+
+---
+[MIT License](https://opensource.org/licenses/MIT)
---
+++ src/scripts/contentscript.js
@@ -0,0 +1,151 @@
+window.addEventListener('load', () => {
+   setTimeout(() => {
+      appendCustomStyles();
+      addQuoteMessage();
+      const observeTarget = document.querySelector('.ts-message-content');
+
+      if (observeTarget) {
+         initializeDOMChangeListener(observeTarget);
+      }
+   }, 4000);
+});
+
+function addQuoteMessage() {
+   const threadBodyMediaList = document.querySelectorAll('[data-tid="threadBodyMedia"]');
+   const sendButton = document.getElementById('send-message-button');
+
+   sendButton.addEventListener('click', sendButtonClickHandler);
+
+   if (threadBodyMediaList) {
+      threadBodyMediaList.forEach(threadBody => {
+         const threadBodyTopRow = threadBody.querySelector('.top-row-text-container');
+
+         if (threadBody && !threadBodyTopRow.hasAttribute('data-hasquote')) {
+            if (threadBodyTopRow) {
+               const quoteMessageButton = document.createElement('button');
+               quoteMessageButton.classList.add('teams-custom-quote-message-button');
+               quoteMessageButton.innerText = 'Reply';
+               Object.assign(quoteMessageButton.style, quoteMessageButtonStyles);
+               threadBodyTopRow.appendChild(quoteMessageButton);
+               threadBodyTopRow.setAttribute('data-hasquote', 'true');
+               quoteMessageButton.addEventListener('click', quoteMessageButtonClickHandler);
+            }
+         }
+      });
+   }
+}
+
+function quoteMessageButtonClickHandler(event) {
+   const messageText = event.target.offsetParent.querySelector('[data-tid="messageBodyContent"]').innerText;
+   const messageSenderName = event.target.offsetParent.querySelector('[data-tid="threadBodyDisplayName"]').innerText;
+
+   const blockquote = document.createElement('blockquote');
+   blockquote.setAttribute('itemtype', 'http://schema.skype.com/Reply');
+
+   const topRowParagraph = document.createElement('p');
+   blockquote.appendChild(topRowParagraph);
+
+   const topRowParagraphBreak = document.createElement('b');
+   topRowParagraphBreak.style.display = 'flex';
+   topRowParagraphBreak.style.justifyContent = 'space-between';
+   topRowParagraph.appendChild(topRowParagraphBreak);
+
+   const nameSpan = document.createElement('span');
+   const closeSpan = document.createElement('span');
+   nameSpan.style.fontSize = 'small';
+   nameSpan.innerText = messageSenderName.trim();
+   closeSpan.innerText = 'x';
+   closeSpan.style.cursor = 'pointer';
+   closeSpan.classList.add('teams-custom-quote-message-close');
+   closeSpan.addEventListener('click', (e) => {
+      replyMessageClickHandler(e, blockquote);
+   });
+   topRowParagraphBreak.appendChild(nameSpan);
+   topRowParagraphBreak.appendChild(closeSpan);
+
+   const textPreviewParagraph = document.createElement('p');
+   textPreviewParagraph.innerText = messageText;
+   blockquote.appendChild(textPreviewParagraph);
+
+   const mainQuoteDiv = document.createElement('div');
+   mainQuoteDiv.appendChild(blockquote);
+
+   const composeTextarea = document.querySelector('.cke_wysiwyg_div');
+   const composePlaceholder = document.querySelector('.ts-text-watermark');
+   composePlaceholder.style.display = 'none';
+   composeTextarea.insertBefore(mainQuoteDiv, composeTextarea.firstChild);
+   
+   const composerParent = document.querySelector('.cke_contents');
+   composerParent.style.height = blockquote.offsetHeight + 40 + 'px';
+}
+
+function replyMessageClickHandler(event, blockquote) {
+   const composer = document.querySelector('.cke_wysiwyg_div');
+   const composerParent = document.querySelector('.cke_contents');
+   const composePlaceholder = document.querySelector('.ts-text-watermark');
+   
+   composer.innerHTML = '<div> <br> </div>';
+   composePlaceholder.style.display = 'block';
+   composerParent.style.height = '33px';
+}
+
+function sendButtonClickHandler() {
+   const replyMessageCloseButton = document.querySelector('.teams-custom-quote-message-close');
+
+   if (replyMessageCloseButton) {
+      replyMessageCloseButton.innerText = '';
+   }
+}
+
+function appendCustomStyles() {
+   const head = document.getElementsByTagName('head')[0];
+   const style = document.createElement('style');
+   const declarations = document.createTextNode('.teams-custom-quote-message-button:hover { color: #a2c0f5 !important; } .teams-custom-quote-message-close:hover { opacity: .5 !important; } ');
+
+   style.type = 'text/css';
+
+   if (style.styleSheet) {
+      style.styleSheet.cssText = declarations.nodeValue;
+   } else {
+      style.appendChild(declarations);
+   }
+
+   head.appendChild(style);
+}
+
+const quoteMessageButtonStyles = {
+   margin: '0 0 0 2em',
+   padding: '0',
+   background: 'transparent',
+   outline: 'none',
+   border: 'none',
+   cursor: 'pointer',
+   fontWeight: 'bold',
+   color: 'cornflowerblue',
+   textDecoration: 'underline'
+}
+
+function initializeDOMChangeListener(observeTarget) {
+   const config = {
+      attributes: false,
+      childList: true,
+      subtree: true,
+      characterData: false,
+      characterDataOldValue: false,
+      attributeOldValue: false,
+   };
+
+   const mutationObserverCallback = function(mutations, observer) {
+      for (let i = 0; i < mutations.length; i++) {
+         for (let x = 0; x < mutations[i].addedNodes.length; x++) {
+            if (mutations[i].addedNodes[x].classList && mutations[i].addedNodes[x].classList.contains('thread-body')) {
+               addQuoteMessage();
+            }
+         }
+      }
+   };
+  
+   const observer = new MutationObserver(mutationObserverCallback);
+   observer.observe(observeTarget, config);
+   console.log('DOM Mutation Observer init');
+}
